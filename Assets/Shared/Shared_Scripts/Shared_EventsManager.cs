e11c84b08aeea772a84fc8ca5d9b3901ffd9686b
Oscar-Skinner <1027514@student.sae.edu.au>
2022-06-17 19:42:56 +1000

    Made Share_EventsManager script, Shared_Mouse_Hold script, CureDog_EventManager and a CureDog_Pour_Particles and CureDog_Timer

---
+++ Assets/Shared/Shared_Scripts/Shared_EventsManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shared_EventsManager : MonoBehaviour
+{
+    //Event to do Hold Mouse Left
+    public delegate void HoldingMouse();
+    public static event HoldingMouse MouseHold;
+
+    public static void HeldMouse()
+    {
+        MouseHold();
+    }
+
+    //Event to do Release Mouse Left
+    public delegate void ReleasedMouse();
+    public static event ReleasedMouse MouseLetGo;
+
+    public static void NoMouseInput()
+    {
+        MouseLetGo();
+    }
+    
+}
