47bb68e2d8c283e1b92a831c947e803b98bcfa3a
DESKTOP-K2D3I7T\Marcus <1023627@student.sae.edu.au>
2022-07-01 21:10:38 +1000

    tried to add the win and lose button

--- Assets/MicroGames/Witch_Maze/Witch_Maze_Scripts/Witch_Maze_VictoryZone.cs
+++ Assets/MicroGames/Witch_Maze/Witch_Maze_Scripts/Witch_Maze_VictoryZone.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Witch_Maze_VictoryZone : MonoBehaviour
 {
+    private bool TimerEnded;
+    private bool WinGame;
+
     // Start is called before the first frame update
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -14,6 +17,41 @@ public class Witch_Maze_VictoryZone : MonoBehaviour
             Debug.Log("Victory!");
         }
 
+    }
+    void TimeDuration()
+    {
+        //if the TimeDuration is called from the event
+        //{
+        //bool TimeEnded changes to true
+        //run the end game function
+        //}
+
+        TimerEnded = true;
+        EndGame();
+    }
+    void EndGame()
+    {
+        //if the player is eligable to win the game and the timer has ended
+        //{
+        //change the scene to the next in order
+        //print the word winner in the console    
+        //}
+        //else
+        //{
+        //print "failed" into the console
+        //reload the scene to try again    
+        //}
+
+        if (WinGame == true && TimerEnded == true)
+        {
+            print("WINNER!");
+            Shared_EventsManager.GameHasBeenWon();
+        }
+        else
+        {
+            print("FAILED!");
+            Shared_EventsManager.GameHasBeenLost();
+        }
     }
 }
 
