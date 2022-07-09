cf061094d01cfae09f20486da4720b50100919fc
Wilso02 <1026304@student.sae.edu.au>
2022-07-09 15:14:25 +1000

    Added a Win/Loss condition to a MicroGame in which also changes scene

--- Assets/MicroGames/MixPotion/Scripts/MixingAbility.cs
+++ Assets/MicroGames/MixPotion/Scripts/MixingAbility.cs
@@ -1,36 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MixingAbility : MonoBehaviour
 {
     public GameObject Spoon;
-
-   
+    public static int score = 0;
     public float movementSpeed = 10f;
+    private bool TimerEnded;
+    private bool WinGame;
+
+    void OnEnable() //enable called event
+    {
+        Shared_EventsManager.EndOfMicroGame += TimerLength; //from the "Shared_EventsManager"
+    }
+    void OnDisable() //disable called event
+    {
+        Shared_EventsManager.EndOfMicroGame -= TimerLength; //from the "Shared_EventsManager"
+    }
+
+    void Start()
+    {
+        TimerEnded = false; //timer hasnt ended
+        WinGame = false; //game hasn't won 
+    }
 
     // Update is called once per frame
     void Update()
     {
 
-        
-
         if (Input.GetKeyDown(KeyCode.A))
         {
             Spoon.transform.position = new Vector3(1f, 0, 0);
-            print("A key was pressed");
+            score += 1;
+            print("A key was pressed Score: " + score);
         }
 
         else if (Input.GetKeyDown(KeyCode.D))
         {
 
             Spoon.transform.position = new Vector3(-1f, 0, 0);
-            print("D key was pressed");
+            score += 1;
+            print("D key was pressed Score: " + score);
+            
 
         }
-        
+   
+    }
 
+    void WinCondition()
+    {
+        if(score >= 15)
+        {
+
+            WinGame = true;
+
+        }
 
 
     }
+
+    void TimerLength()
+    {
+
+        TimerEnded = true;
+        EndGame();
+
+    }
+
+    void EndGame()
+    {
+        if (WinGame == true && TimerEnded == true)
+        {
+            print("Winner!");
+            SceneManager.LoadScene(9);
+
+
+        }
+        else
+        {
+
+            print("Failure!");
+            SceneManager.LoadScene(10);
+
+        }
+    }
+    
 }
