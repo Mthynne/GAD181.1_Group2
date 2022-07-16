1ccacfce34a1345aac89c81e07666026d6ab45aa
Oscar-Skinner <1027514@student.sae.edu.au>
2022-07-16 14:20:22 +1000

    Created a shared lives system for the Dog Beaty Competition. Next time I work on them Ill add it to everyones games.

--- Assets/MicroGames/MainMenu/MainMenu_Scripts/MainManager_SceneManager.cs
+++ Assets/MicroGames/MainMenu/MainMenu_Scripts/MainManager_SceneManager.cs
@@ -9,6 +9,7 @@ public class MainManager_SceneManager : MonoBehaviour
     {
         //when the play button is pressed chang ethe scene to the next one in the order
         //print in the console so we knwo it works
+        Shared_EventsManager.GameHasBeenWon();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
         print("Play the game");
     } 
---
+++ Assets/Shared/Shared_Scripts/Shared_PlayerLives.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Shared_PlayerLives : MonoBehaviour
+{
+    private int playersLives;
+
+    public GameObject livesUI_1;
+    public GameObject livesUI_2;
+    public GameObject livesUI_3;
+
+    void OnEnable()
+    {
+        Shared_EventsManager.CompleteGame += IncreaseLives;
+        Shared_EventsManager.LostTheGame += DecreaseLives;
+    }
+    void OnDisable()
+    {
+        Shared_EventsManager.CompleteGame -= IncreaseLives;
+        Shared_EventsManager.LostTheGame -= DecreaseLives;
+    }
+
+    void Awake()
+    {
+        //set int playerlives to obtained pref int;
+        playersLives = PlayerPrefs.GetInt("Lives");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(playersLives == 3)
+        {
+            ThreeLivesDisplay();
+        }
+        else if(playersLives == 2)
+        {
+            TwoLivesDisplay();
+        }
+        else if(playersLives == 1)
+        {
+            OneLivesDisplay();
+        }
+    }
+
+    void ThreeLivesDisplay()
+    {
+        livesUI_1.SetActive(true);
+        livesUI_2.SetActive(true);
+        livesUI_3.SetActive(true);
+    }
+
+    void TwoLivesDisplay()
+    {
+        livesUI_1.SetActive(true);
+        livesUI_2.SetActive(true);
+        livesUI_3.SetActive(false);
+    }
+
+    void OneLivesDisplay()
+    {
+        livesUI_1.SetActive(true);
+        livesUI_2.SetActive(false);
+        livesUI_3.SetActive(false);
+    }
+
+    void IncreaseLives()
+    {
+        //set player pref to int 3;
+    	PlayerPrefs.SetInt("Lives", 3);
+    }
+
+    void DecreaseLives()
+    {
+        playersLives--;
+        print(playersLives);
+        PlayerPrefs.SetInt("Lives", playersLives);
+        LoseScreen();
+    }
+
+    void LoseScreen()
+    {
+        if(playersLives <= 0)
+        {
+            PlayerPrefs.SetInt("Lives", 3);
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+}
