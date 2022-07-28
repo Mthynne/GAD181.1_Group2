f9430178191cfd3761d39b328ee51fa423b35b98
Mthynne <1027517@student.sae.edu.au>
2022-07-28 17:37:25 +1000

    Finished match game

--- Assets/MicroGames/Match_Ingredients/MatchIngredientsManager.cs
+++ Assets/MicroGames/Match_Ingredients/MatchIngredientsManager.cs
@@ -1,26 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MatchIngredientsManager : MonoBehaviour
 {
     private int correctAnswer;
 
-    public GameObject[] Apple, Orange, Tomato, Cherry, Lime;
+    public Sprite[] mySprites;
     //I will likely need: a public array of sprites
 
-    public SpriteRenderer spriteRenderer;
+    public float success =0f;
 
-    void Start()
+    private bool WinGame = false;
+    private bool TimerEnded = false;
+  
+
+    void OnEnable()
     {
-        correctAnswer = Random.Range(1, 6);
-        // Look into how to change a ui image sprite in code
+        Shared_EventsManager.EndOfMicroGame += TimerLength; //from the "Shared_EventsManager"
+    }
 
-
+    void OnDisable()
+    {
+        Shared_EventsManager.EndOfMicroGame -= TimerLength; //from the "Shared_EventsManager"
     }
 
 
+    void Start()
+    {
+        correctAnswer = Random.Range(0, 5);
+        // Look into how to change a ui image sprite in code
 
+        this.GetComponent<Image>().sprite = mySprites[correctAnswer];
+
+
+    }
 
     public void PlayerAnswer(int answerButtonID)
     {
@@ -28,10 +43,49 @@ public class MatchIngredientsManager : MonoBehaviour
 
         if(answerButtonID == correctAnswer)
         {
-            Debug.Log("You win");
+            success++;
+            Debug.Log(success);
+            
+            if(success >= 5)
+            {
+                Debug.Log("Win");
+                WinCondition();
+            }
+            else
+            {
+                Start();
+            }
+            
         }
     }
 
+    void WinCondition()
+    {
+        Shared_EventsManager.GameHasBeenWon();
+        WinGame = true;
+    }
 
+
+    void TimerLength()
+    {
+        TimerEnded = true;
+        EndGame();
+    }
+
+    void EndGame()
+    {
+        if (WinGame == true && TimerEnded == true)
+        {
+            print("Winner!");
+            Shared_EventsManager.GameHasBeenWon();
+        }
+        else
+        {
+            print("Failure!");
+            Shared_EventsManager.GameHasBeenLost();
+        }
+    }
     // if i want a new question, i only need to run start again, if i want to count successful answers i will need a variable that is not set in start
+
+
 }
