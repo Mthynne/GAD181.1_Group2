bf771c52cd92cec87dba355e18dddbe222895179
Oscar-Skinner <1027514@student.sae.edu.au>
2022-08-03 19:03:55 +1000

    Starting to work on the Beauty Comp Assets

--- Assets/MicroGames/DogBeautyComp_MicroGame/DogBeautyComp_Scripts/DogBeautyComp_MovementSpice.cs
+++ Assets/MicroGames/DogBeautyComp_MicroGame/DogBeautyComp_Scripts/DogBeautyComp_MovementSpice.cs
@@ -1,17 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DogBeautyComp_MovementSpice : MonoBehaviour
 {
     private bool spinningEntertainment = true;
 
+    private int Clicks = 0;
+    public TextMeshProUGUI clapScore;
+
+    void OnEnable()
+    {
+        DogBeautyComp_EventsManager.CheerTheDogOn += SwitchingSides;
+    }
+    void OnDisable()
+    {
+        DogBeautyComp_EventsManager.CheerTheDogOn -= SwitchingSides;
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(spinningEntertainment == true)
+        if(Clicks == 3 || Clicks == 6 || Clicks == 9)
         {
-            this.transform.Rotate(new Vector3(45, 90, -90) * Time.deltaTime);
+            this.transform.Rotate(new Vector3(90, 180, 0));
+        }
+        else if(Clicks == 1 || Clicks == 4 || Clicks == 7)
+        {
+            this.transform.Rotate(new Vector3(90, 0, 0));
         }
     }
+
+    void SwitchingSides()
+    {
+        Clicks++;
+        clapScore.text = "Claps: " + Clicks;
+    }
+
 }
