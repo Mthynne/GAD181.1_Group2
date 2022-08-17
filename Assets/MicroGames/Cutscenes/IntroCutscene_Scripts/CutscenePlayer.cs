cfea1b931d7f62db33a06390a70444c60a72206f
Oscar-Skinner <1027514@student.sae.edu.au>
2022-08-17 21:45:19 +1000

    Butttons for every scene work now

--- Assets/MicroGames/Cutscenes/IntroCutscene_Scripts/CutscenePlayer.cs
+++ Assets/MicroGames/Cutscenes/IntroCutscene_Scripts/CutscenePlayer.cs
@@ -20,9 +20,22 @@ public class CutscenePlayer : MonoBehaviour
         IntroCutsceneVid.loopPointReached += CheckOver; //this checks to see if its at the loop point and when it is it will start CheckOver().
     }
 
+    void Update()
+    {
+        if(Input.GetKeyDown("escape"))
+        {
+            SkipCutscene();
+        }
+    }
+
     void CheckOver(UnityEngine.Video.VideoPlayer vp)
     {
         //change the scene when the video reaches the end.
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void SkipCutscene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);        
     }
 }
